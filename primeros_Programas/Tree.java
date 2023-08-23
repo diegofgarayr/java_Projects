0a34ec08f7f85d1327825b9b18e20db31a0367b1
diegofgarayr <143001242+diegofgarayr@users.noreply.github.com>
2023-08-23 09:03:51 -0500

    Add files via upload

---
+++ primeros_Programas/Tree.java
@@ -0,0 +1,20 @@
+
+package tree;
+
+
+public class Tree  
+{    
+    public static void main(String args[]) {  
+       System.out.println("hola por alla soy diego y estoy aprendiendo a programa en java ");
+       System.out.println("voy a plantar un arbol o  voy a ensuciarme las manos");
+       System.out.println("   *   ");
+       System.out.println("  ***  ");
+       System.out.println("*******");
+       System.out.println(" *****  ");
+       System.out.println("  ***  ");
+       System.out.println("   *  ");
+       System.out.println("   *  ");
+       System.out.println("   *  ");
+       System.out.println("   *  ");
+   }  
+}  
