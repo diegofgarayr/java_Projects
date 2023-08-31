0c7c8c93f0eb6e8883d17b521bc9953de70f0270
diegofgarayr <143001242+diegofgarayr@users.noreply.github.com>
2023-08-31 09:22:00 -0500

    Add files via upload

---
+++ primeros_Programas/While_example.java
@@ -0,0 +1,15 @@
+
+package while_example;
+
+
+public class While_example {
+public static void main(String[] args) { 
+    int i=1;  
+    while(i<=10){  
+        System.out.println(i);  
+    i++;  
+    } 
+}
+}
+    
+    
