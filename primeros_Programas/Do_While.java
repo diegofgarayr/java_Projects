adde476f696d4b01770031862af0d91c6731aacb
diegofgarayr <143001242+diegofgarayr@users.noreply.github.com>
2023-08-31 09:26:18 -0500

    Add files via upload

---
+++ primeros_Programas/Do_While.java
@@ -0,0 +1,16 @@
+
+package do_while;
+
+public class Do_While {
+
+    public static void main(String[] args) {
+       int i=1;    
+    do{    
+        System.out.println(i);    
+    i++;    
+    }while(i<=10);    
+}    
+}  
+    
+    
+
