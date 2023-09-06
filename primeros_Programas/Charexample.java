b26e1547f10074bf7d8188ad0c037388d903ef2c
diegofgarayr <143001242+diegofgarayr@users.noreply.github.com>
2023-09-06 08:47:47 -0500

    Add files via upload

---
+++ primeros_Programas/Charexample.java
@@ -0,0 +1,11 @@
+
+package charexample;
+
+
+public class Charexample {
+ 
+	public static void main(String[] args) {   
+     char expectedGrade = 'C'; 
+     System.out.println(expectedGrade);
+	}
+}
