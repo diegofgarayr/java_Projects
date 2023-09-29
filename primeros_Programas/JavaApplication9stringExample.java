5d34648c8a47ac113fe8a4b455bf2e437b74ba4d
diegofgarayr <143001242+diegofgarayr@users.noreply.github.com>
2023-09-29 10:19:57 -0500

    Add files via upload

---
+++ primeros_Programas/JavaApplication9stringExample.java
@@ -0,0 +1,13 @@
+
+package javaapplication9stringexample;
+
+
+public class JavaApplication9stringExample {
+
+    
+    public static void main(String[] args) {
+        String openingLyrics = "Yesterday, all my troubles seemed so far away";
+    System.out.println(openingLyrics);
+    }
+    
+}
---
+++ primeros_Programas/Variables1.java
@@ -0,0 +1,13 @@
+
+package variables1;
+
+public class Variables1 {
+
+    public static void main(String[] args) {
+       String name = "Diego Garay";
+    int yearCreated = 2005;     
+    System.out.println(name);
+    System.out.println(yearCreated);
+    }
+    
+}
