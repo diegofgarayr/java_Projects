c998800eb71bb839843f403f8a8ed4f395ced1b5
diegofgarayr <143001242+diegofgarayr@users.noreply.github.com>
2023-10-05 09:47:28 -0500

    Add files via upload

---
+++ Segundo_Corte/Herencia/TestHerencia.java
@@ -0,0 +1,11 @@
+
+package test;
+
+import herencia.*;
+public class TestHerencia {
+    public static void main(String[] args) {
+      Persona persona1=new Persona("Diego Garay ",'H',18,"Cundinamarca")  ;
+        System.out.println(persona1);
+    }
+    
+}
