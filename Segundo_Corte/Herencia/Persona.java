4a85797fd1c7cc4cc327e84834508f7ca3729b52
diegofgarayr <143001242+diegofgarayr@users.noreply.github.com>
2023-10-18 09:24:17 -0500

    Add files via upload

--- Segundo_Corte/Herencia/Persona.java
+++ Segundo_Corte/Herencia/Persona.java
@@ -1,20 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package herencia;
+package herenci;
 
-/**
- *
- * @author Estudiantes
- */
-public class Herencia {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Persona{
+    
+    protected String nombre;
+    protected char genero;
+    protected int edad;
+    protected String direccion;
+    
+    public Persona(){
+        
     }
     
+    public Persona (String nombre, char genero, int edad, String direccion){
+        this.nombre = nombre;
+        this.genero = genero;
+        this.edad = edad;
+        this.direccion = direccion;
+    }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+        
+    public void setNombre (String nombre){
+        this.nombre = nombre;
+    }
+    public char getGenero (){
+        return this.genero;
+    }
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+    public int getEdad() {
+        return this.edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public String getDireccion() {
+        return this.direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Persona{nombre=").append(this.nombre);
+        sb.append(", genero=").append(this.genero);
+        sb.append(", edad=").append(this.edad);
+        sb.append(", direccion=").append(this.direccion);
+        sb.append('}');
+        return sb.toString();
+    }   
 }
