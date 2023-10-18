ebfbf367d3bca16ac461ee6f282c85da6400a462
diegofgarayr <143001242+diegofgarayr@users.noreply.github.com>
2023-10-18 10:02:42 -0500

    Add files via upload

--- Segundo_Corte/Herencia/TestHerencia.java
+++ Segundo_Corte/Herencia/TestHerencia.java
@@ -1,11 +1,20 @@
-
 package test;
 
-import herencia.*;
+import herenci.*;
+
 public class TestHerencia {
+
     public static void main(String[] args) {
-      Persona persona1=new Persona("Diego Garay ",'H',18,"Cundinamarca")  ;
-        System.out.println(persona1);
+
+        //Crear empleados 
+        Empleado empleado1 = new Empleado("Alex", 3000);
+        System.out.println(empleado1);
+        Empleado empleado2 = new Empleado("Reol", 4000);
+        System.out.println(empleado2);
+
+        Empleado empleado3 = new Empleado("Juan", 5000);
+        System.out.println(empleado3);
+
+       
     }
-    
 }
