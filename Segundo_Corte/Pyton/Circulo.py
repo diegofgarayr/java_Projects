c6241d04e8eae21ba23bf069d70ac68afcd53eb1
diegofgarayr <143001242+diegofgarayr@users.noreply.github.com>
2023-10-25 09:55:37 -0500

    Add files via upload

--- Segundo_Corte/Pyton/Circulo.py
+++ Segundo_Corte/Pyton/Circulo.py
@@ -5,11 +5,17 @@ class Circulo:
          self.radius=diameter/2
 
     def area (self):
-         return self.pi*self.radius**2
+         return self.pi*self.radius ** 2
 
     def perimeter(self):
-        return self.pi*self.radius**2
+        return self.pi*2 *self.radius
+
+    
 
 media_pizza=Circulo(12)
 print("Area media pizza:",media_pizza.area())
 print("perimetro media pizza:",media_pizza.perimeter())
+lote=Circulo(10)
+print("Area lote:",lote.area())
+print("perimetro lote:",lote.perimeter())
+    
